e16633df6f66cf35c3ee84f28fb3a038fa6d9666
v.shago <dpatriot@gmail.com>
2016-01-26 14:43:54 +0300

    no message

--- packages.sh
+++ packages.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo R --no-save << EOF
-install.packages(c('RJDBC', 'doParallel', 'foreach', 'plyr', 'data.table'),
+apt-get update && apt-get install -y libcurl4-openssl-dev apt-get install libxml2-dev libdigest-hmac-perl
+
+R CMD javareconf
+
+R --no-save << EOF
+install.packages(c('RCurl', 'XML', 'digest', 'RJDBC', 'doParallel', 'foreach', 'plyr', 'data.table'),
 repos="http://cran.rstudio.com", INSTALL_opts=c('--byte-compile') )
 EOF
 
-sudo R --no-save << EOF
+R --no-save << EOF
 install.packages(c('RAmazonS3'),
 repos="http://www.omegahat.org/R", INSTALL_opts=c('--byte-compile') )
 EOF
